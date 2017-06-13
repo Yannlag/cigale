83f04578459c8cd42e77dc3bf3d7a63e04338232
Unknown <yannick@cigalecommunication.fr>
2017-06-13 16:23:31 +0200

    integrate modifs

--- server.js
+++ server.js
@@ -1,139 +1,108 @@
+console.log( '/******************************************' );
+console.log( ' ' );
+
 var Botkit = require( 'botkit' );
 var timer = {};
 var timerCount = 0;
+var emailRequestTimer = {};
+var archiveTimer = {};
 var express = require( 'express' );
 var path = require( 'path' );
 var app = express();
 var server = require( 'http' ).createServer();
+var Sellsy = require( 'node-sellsy' );
+var guestCount = 3;
+var botIdentity = {};
+var TIME_BEFORE_EMAIL_REQUEST = 10;
+var TIME_BEFORE_ARCHIVE_CHANNEL = 15;
+var debug = true;
 
-var WebSocketServer = require( 'ws' ).Server;
-var wss = new WebSocketServer( { server: server } );
+var context = {
+    sellsyTicketid: ''
+};
 
-wss.on( 'connection', function( ws ) {
-    var id = setInterval( function() {
-        ws.send( JSON.stringify( process.memoryUsage() ), function() { /* ignore errors */ } );
-    }, 100 );
-    console.log( 'started client interval' );
-    ws.on( 'close', function() {
-        console.log( 'stopping client interval' );
-        clearInterval( id );
-    } );
+var sellsy = new Sellsy( {
+    creds: {
+        consumerKey: '5dd2c2992ab7c0fd540ab7212b78f57dd21bc502',
+        consumerSecret: '7f732377b191924165f787876031ffbfe830195d',
+        userToken: 'bf64efe8686757345c8178768c69cc134c37230b',
+        userSecret: '657ca7ba0858f286fdec8478efb42d8fb7036732'
+    }
+} );
+
+var botToken = 'xoxb-175444252721-kr6junoMyy0KNxtkwoktzW4H';
+var appToken = 'xoxp-164235888548-164199315090-175758624274-38f00700004614b0fdb5ee5675fc7f30';
+
+var appController = Botkit.slackbot( {
+    // debug: false
+} );
+var appSlack = appController.spawn( {
+    token: appToken
 } );
 
 var controller = Botkit.slackbot( {
-    debug: true,
+    // debug: false
 } );
 
 var bot = controller.spawn( {
-    token: process.env.SLACK_TOKEN
-} ).startRTM();
-
-controller.hears( [ 'hello', 'hi' ], 'direct_message,direct_mention,mention,message_received,ambient', function( bot, message ) {
-    bot.reply( message, 't\'as un problème ?' );
-    stopTimer();
-    startTimer( 10, function() {
-        bot.reply( message, 'test' );
-    } );
-
-
-    // bot.api.reactions.add( {
-    //     timestamp: message.ts,
-    //     channel: message.channel,
-    //     name: 'robot_face',
-    // }, function( err, res ) {
-    //     if ( err ) {
-    //         bot.botkit.log( 'Failed to add emoji reaction :(', err );
-    //     }
-    // } );
-
-
-    // controller.storage.users.get( message.user, function( err, user ) {
-    //     if ( user && user.name ) {
-    //         bot.reply( message, 'Hello ' + user.name + '!!' );
-    //     } else {
-    //         bot.reply( message, 'Hello.' );
-    //     }
-    // } );
+    token: botToken
+} ).startRTM( function( err, bot, payload ) {
+    botIdentity = bot.identifyBot();
 } );
 
-controller.hears( 'stop', 'ambient', function( bot, message ) {
-    stopTimer();
-    bot.reply( message, 'timer stopped' );
-} )
+var WebSocketServer = require( 'ws' ).Server;
+var wss = new WebSocketServer( { server: server } );
+var clients = [];
 
+wss.on( 'connection', function( ws ) {
 
-
-controller.hears( [ 'shutdown' ], 'direct_message,direct_mention,mention', function( bot, message ) {
-
-    bot.startConversation( message, function( err, convo ) {
-
-        convo.ask( 'Are you sure you want me to shutdown?', [
-            {
-                pattern: bot.utterances.yes,
-                callback: function( response, convo ) {
-                    convo.say( 'Bye!' );
-                    convo.next();
-                    setTimeout( function() {
-                        process.exit();
-                    }, 3000 );
-                }
-            },
-            {
-                pattern: bot.utterances.no,
-                default: true,
-                callback: function( response, convo ) {
-                    convo.say( '*Phew!*' );
-                    convo.next();
-                }
-        }
-        ] );
+    ws.on( 'close', function( closeEvent ) {
+        clients.splice(
+            clients.findIndex(
+                function( element ) {
+                    return element.channel == this;
+                },
+                'C5B9GMT8W'
+            ), 1 );
+        // console.log( clients );
     } );
+    ws.on( 'message', function( data, flags ) {
+        onClientMessage( this, data );
+    } );
+
+    // console.log( 'clients : ' + clients );
 } );
 
-
-controller.hears( [ 'uptime', 'identify yourself', 'who are you', 'what is your name' ],
-    'direct_message,direct_mention,mention',
-    function( bot, message ) {
-
-        var hostname = os.hostname();
-        var uptime = formatUptime( process.uptime() );
-
-        bot.reply( message,
-            ':robot_face: I am a bot named <@' + bot.identity.name +
-            '>. I have been running for ' + uptime + ' on ' + hostname + '.' );
-
-    } );
-
-function formatUptime( uptime ) {
-    var unit = 'second';
-    if ( uptime > 60 ) {
-        uptime = uptime / 60;
-        unit = 'minute';
+controller.on( 'ambient', function( bot, message ) {
+    if ( debug ) console.log( 'get message from Slack' );
+    if ( debug ) console.log( 'clients list : ', clients );
+    var WS = clients.find( function( element ) {
+        return element.channel == this;
+    }, message.channel );
+    if ( !isGuestMessage( message ) ) {
+        if ( debug ) console.log( 'message has been sent by Support' );
+        stopEmailRequestTimer();
+        stopArchiveTimer();
+        startArchiveTimer( message.channel );
     }
-    if ( uptime > 60 ) {
-        uptime = uptime / 60;
-        unit = 'hour';
-    }
-    if ( uptime != 1 ) {
-        unit = unit + 's';
-    }
+    if ( debug ) console.log( 'addSellsyTicketReply' );
 
-    uptime = uptime + ' ' + unit;
-    return uptime;
-}
+    addSellsyTicketReply( message.channel, 'note', message.text, function() {} );
 
-function startTimer( limit, callback ) {
-    timerCount++;
-    if ( timerCount < limit ) {
-        timer = setTimeout( startTimer.bind( null, limit, callback ), 1000 )
-    } else {
-        callback();
-    }
+    WS.ws.send( JSON.stringify( {
+            type: 'message',
+            message: message.text
+        } ),
+        function() {}
+    );
+} );
+
+function delayFonction( duration, toExecute ) {
+    return setTimeout( toExecute, duration * 1000 );
 }
 
 function stopTimer() {
     clearTimeout( timer );
-    timerCount = 0;
 }
 
 var express = require( 'express' )
@@ -155,11 +124,13 @@ var twilio = require( 'twilio' );
 app.use( bodyParser.json() ); // to support JSON-encoded bodies
 app.use( bodyParser.urlencoded( { extended: true } ) ); // to support URL-encoded bodies
 
-app.get( '/voice/:clientId', function( request, response ) {
-    response.type( 'text/xml' );
-    if ( request.params.clientId ) {
-        response.send( '<Response><Dial><Client>' + request.params.clientId + '</Client></Dial></Response>' );
-    }
+app.post( '/message', function( request, response ) {
+    var channel = request.body.channel;
+    var message = request.body.message;
+    bot.sendWebhook( {
+        text: message,
+        channel: channel
+    } );
 } );
 
 
@@ -167,3 +138,201 @@ server.on( 'request', app );
 server.listen( 8080, function() {
     console.log( 'Listening on http://localhost:8080' );
 } );
+
+
+function isGuestMessage( message ) {
+    appSlack.api.users.info( {
+            user: message.user
+        },
+        function( err, response ) {
+            if ( !err ) {
+                return response.user.name.slice( 0, 5 ) == 'guest';
+            }
+        }
+    );
+}
+
+function createTicket( message, callback ) {
+    if ( debug ) console.log( 'launch new Sellsy ticket creation' );
+    var params = {
+        ticket: {
+            subject: 'From Slack widget',
+            requesterEmail: 'none@none.com',
+            message: message
+        }
+    };
+
+    sellsy.api( {
+        method: 'Support.create',
+        params: params
+    } ).then( data => {
+        callback( data.response.ticketid );
+        if ( debug ) console.log( 'new sellsy ticket : ' + data.response.ticketid );
+    } ).catch( e => {
+        console.log( 'error:', e );
+    } );
+}
+
+function addSellsyTicketReply( channelId, type, message, callback ) {
+    getChannelName( channelId, function( ticketid ) {
+        var params = {
+            ticketid: ticketid,
+            reply: {
+                type: type,
+                step: 'active',
+                message: message,
+            }
+        };
+        if ( debug ) console.log( 'Variables addSellsyTicketReply : ', { ticketid: ticketid, type: type, message: message } );
+        sellsy.api( {
+            method: 'Support.reply',
+            params: params
+        } ).then( data => {
+            callback();
+        } ).catch( e => {
+            console.log( 'error while adding Sellsy ticket reply : ', e );
+        } );
+    } );
+}
+
+function getChannelName( channelId, callback ) {
+    appSlack.api.channels.info( {
+            channel: channelId
+        },
+        function( err, response ) {
+            if ( !err ) {
+                callback( response.channel.name );
+            }
+        }
+    );
+}
+
+function archiveChannel( channelId ) {
+    appSlack.api.channels.archive( {
+        channel: channelId
+    } );
+}
+
+function checkChannelStatus( channelId, callback ) {
+    appSlack.api.channels.info( {
+            channel: channelId
+        },
+        function( err, response ) {
+            if ( !err ) {
+                if ( response.channel.is_archived ) {
+                    if ( debug ) console.log( 'channel ' + channelId + ' is archived' );
+                    unArchiveChannel( channelId, callback );
+                } else {
+                    callback();
+                }
+            }
+        } );
+}
+
+function unArchiveChannel( channel, callback ) {
+    appSlack.api.channels.unarchive( {
+            channel: channel
+        },
+        function( err, response ) {
+            callback();
+            if ( debug ) console.log( 'channel ' + channel + ' has been unarchived' );
+            inviteBot( channel );
+        }
+    );
+}
+
+function onClientMessage( ws, data ) {
+    stopArchiveTimer();
+    data = JSON.parse( data );
+    var channel = '';
+    if ( data.channel == '' ) {
+        if ( debug ) console.log( 'no channel # sent by client' );
+        createTicket( data.message, function( channelName ) {
+            context.sellsyTicketid = channelName;
+            appSlack.api.channels.create( {
+                    name: channelName
+                },
+                function( err, response ) {
+                    // console.log( response )
+                    if ( err ) {
+                        console.log( err );
+                    } else {
+                        clients.push( {
+                            id: '1',
+                            channel: response.channel.id,
+                            ws: ws
+                        } );
+                        ws.send(
+                            JSON.stringify( {
+                                type: 'connection',
+                                channel: response.channel.id
+                            } )
+                        );
+                        // console.log( 'bot-creation' )
+
+                        inviteBot( response.channel.id );
+                        sendMessageToSlack( response.channel.id, data.message );
+                        // startEmailRequestTimer( ws );
+                    }
+                } );
+        } );
+    } else {
+        if ( debug ) console.log( 'client has sent channel # : ' + data.channel );
+        checkChannelStatus( data.channel, function() {
+            sendMessageToSlack( data.channel, data.message );
+            // startEmailRequestTimer( ws );
+            addSellsyTicketReply( data.channel, 'email', data.message, function() {} );
+        } );
+    }
+}
+
+function sendMessageToSlack( channel, message ) {
+    if ( debug ) console.log( 'send message ' + message + ' to ' + channel );
+    bot.say( {
+        channel: channel,
+        text: message,
+        username: 'Guest'
+    } );
+}
+
+function startEmailRequestTimer( ws ) {
+    if ( debug ) console.log( 'start Email Request Timer' );
+    emailRequestTimer = setTimeout( function() {
+        ws.send( JSON.stringify( {
+                type: 'mailRequest'
+            } ),
+            function() {
+
+            } );
+
+    }, TIME_BEFORE_EMAIL_REQUEST * 1000 );
+}
+
+function stopEmailRequestTimer() {
+    clearTimeout( emailRequestTimer );
+}
+
+function startArchiveTimer( channel ) {
+    if ( debug ) console.log( 'start Archive Timer' );
+    archiveTimer = setTimeout( function() {
+        archiveChannel( channel );
+    }, TIME_BEFORE_ARCHIVE_CHANNEL * 1000 );
+}
+
+function stopArchiveTimer() {
+    if ( archiveTimer !== undefined ) clearTimeout( archiveTimer );
+}
+
+function inviteBot( channel ) {
+    appSlack.api.channels.invite( {
+        channel: channel,
+        user: botIdentity.id
+    }, function( err, response ) {
+        if ( err ) {
+            console.log( err );
+        } else {
+            if ( debug ) console.log( 'Bot invited to channel ' + channel );
+            // console.log( response );
+        }
+    } );
+}
